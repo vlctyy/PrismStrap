2ee7bee63e2bb4db73a2b37eaea57d04cae1917b
MaxlaserTech <135626624+MaxlaserTechAlt@users.noreply.github.com>
2024-11-04 18:23:48 +0700

    Update GetFFlag.lua

--- Main/Functions/GetFFlag.lua
+++ Main/Functions/GetFFlag.lua
@@ -1,5 +1,4 @@
-local function GetFFlag(flag: string, value: string?)
-	local type = type or typeof
+return function(flag: string, value: string?): (string, string?) -> (string?)
 	if type(flag) ~= "string" then return task.spawn(error, "string expected, got "..type(flag)) end
 	local FFlag: string = Bloxstrap.TouchEnabled and flag:gsub("DFInt", ""):gsub("DFFlag", ""):gsub("FFlag", ""):gsub("FInt", ""):gsub("DFString", ""):gsub("FString", "") or flag --> Removes the keyword of the FFlag, setfflag doesn't like those so we will need to remove it.
 	
@@ -9,4 +8,3 @@ local function GetFFlag(flag: string, value: string?)
 		return task.spawn(error, "FFlag expected, got "..FFlag)
 	end
 end
-return GetFFlag
