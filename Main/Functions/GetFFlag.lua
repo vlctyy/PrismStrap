0e7bdc04b8a1ac1bc654ff5c7896fd27e9c62c5e
qwertyui <watchdogbestac@gmail.com>
2024-11-04 04:44:39 -0600

    Add files via upload

--- Main/Functions/GetFFlag.lua
+++ Main/Functions/GetFFlag.lua
@@ -1,12 +1,12 @@
-local function ToggleFFlag(flag: string, value: any)
+local function GetFFlag(flag: string, value: string?)
 	local type = type or typeof
 	if type(flag) ~= "string" then return task.spawn(error, "string expected, got "..type(flag)) end
 	local FFlag: string = Bloxstrap.TouchEnabled and flag:gsub("DFInt", ""):gsub("DFFlag", ""):gsub("FFlag", ""):gsub("FInt", ""):gsub("DFString", ""):gsub("FString", "") or flag --> Removes the keyword of the FFlag, setfflag doesn't like those so we will need to remove it.
 	
 	if getfflag(FFlag) ~= nil then
-		return setfflag(FFlag, tostring(value))
+		return getfflag(FFlag)
 	else
 		return task.spawn(error, "FFlag expected, got "..FFlag)
 	end
 end
-return ToggleFFlag
+return GetFFlag
