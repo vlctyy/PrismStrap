7b1dc71812c814c5db2a14383aca583715f7ed50
qwertyui <watchdogbestac@gmail.com>
2024-11-04 04:43:18 -0600

    Add files via upload

---
+++ Initiate.lua
@@ -0,0 +1,11 @@
+--[[
+Bloxstrap: Executor Edition
+
+Initiate.lua -> Starts the script, and enabled FFlags and modifications.
+
+Credits:
+-> qwertyui: main dev
+]]--
+
+local Bloxstrap: table = loadfile("Bloxstrap/Main/Bloxstrap.lua")() --> Loars all the functions required to use Bloxstrap
+Bloxstrap.start() --> Begins the actual script
