9e073aa87bbedf4799032854dc0efd1099a8c80d
MaxlaserTech <135626624+Maxlasertech@users.noreply.github.com>
2025-01-30 01:20:52 +0700

    Update toggleflag.lua

--- modules/libraries/toggleflag.lua
+++ modules/libraries/toggleflag.lua
@@ -1,14 +1,21 @@
 local cloneref = cloneref or function(...) return ... end
-local HttpService = cloneref(game.GetService(game, "HttpService"))
-return function(flag: string, value: string): (string, string) -> ()
-	local type = type or typeof
-	flag = tostring(flag)
-	local FFlag: string = cloneref(game:GetService('UserInputService')).TouchEnabled and flag:gsub("DFInt", ""):gsub("DFFlag", ""):gsub("FFlag", ""):gsub("FInt", ""):gsub("DFString", ""):gsub("FString", "") or flag --> Removes the keyword of the FFlag, setfflag doesn't like those so we will need to remove it.
-	
-	if getfflag(FFlag) ~= nil then
-		local fflagfile = HttpService:JSONDecode(readfile("Bloxstrap/modules/configuration/fastflags.json"))
-		fflagfile[flag] = tostring(value)
-		writefile("Bloxstrap/modules/configuration/fastflags.json", HttpService:JSONEncode(fflagfile))
-		return setfflag(FFlag, tostring(value))
-	end
-end;
+local HttpService = cloneref(game.GetService(game, 'HttpService'))
+
+return function(fastflag, value)
+    local fastflagnew = cloneref(game:GetService('UserInputService')).TouchEnabled and fastflag:gsub('DFInt', ''):gsub('DFaFlag', ''):gsub('FFlag', ''):gsub('FInt', ''):gsub('DFString', ''):gsub('FString', '') or fastflag :: string
+    if ({pcall(function() return getfflag(fastflagnew) end)})[1] and value ~= nil then
+        local newvalue = {} :: table
+        for i,v in HttpService:JSONDecode(readfile('Bloxstrap/modules/configuration/fastflags.json')) do
+            newvalue[i] = v
+        end
+        print(value, tostring(value))
+        newvalue[fastflag] = tostring(value)
+        writefile('Bloxstrap/modules/configuration/fastflags.json', HttpService:JSONEncode(newvalue))
+        return setfflag(fastflagnew, tostring(value))
+    elseif value ~= nil and not ({pcall(function() return getfflag(fastflagnew) end)})[1] then
+        if not isfile('Bloxstrap/cache/logs.txt') then
+            writefile('Bloxstrap/cache/logs.txt', '')
+        end
+        appendfile('Bloxstrap/cache/logs.txt', `Attempted to use an unsupported fastflag ({fastflag}), Value: {value}.\n`)
+    end
+end
