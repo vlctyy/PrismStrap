854c1e10c0ed250bfa76a9e49a5582badb5d018a
MaxlaserTech <135626624+Maxlasertech@users.noreply.github.com>
2025-03-13 02:54:05 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -27,6 +27,9 @@ if not isfolder('bloxstrap') or not isfolder('bloxstrap/logs') then
     writefile('bloxstrap/main.lua', `loadstring(game:HttpGet('https://raw.githubusercontent.com/qwertyui-is-back/Bloxstrap/refs/heads/main/main.lua', true))()`)
     writefile('bloxstrap/audios/oof sound.mp3', game:HttpGet('https://raw.githubusercontent.com/qwertyui-is-back/Bloxstrap/refs/heads/main/audios/oof sound.mp3', true))
 end
+if not isfile('bloxstrap/logs/profile.json') the 
+    writefile('bloxstrap/logs/profile.json', '{}')
+end
 if not isfile('bloxstrap/selected.txt') then
     writefile('bloxstrap/selected.txt', 'fluent')
 end
