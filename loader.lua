1d30674cc5f48dc8c0f5d7e14763436ca6770202
MaxlaserTech <135626624+MaxlaserTechAlt@users.noreply.github.com>
2025-04-01 15:59:18 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local cloneref = (table.find({'Xeno', 'Fluxus'}, identifyexecutor(), 1) or not cloneref) and function(ref)
     return ref
 end or cloneref :: (any)
-getgenv().developer = true
+--getgenv().developer = true
 getgenv().error = function(msg, lvl)
     appendfile('bloxstrap/logs/error.txt', `{msg}\n`)
     --if getgenv().developer then
