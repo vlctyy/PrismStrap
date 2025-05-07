e738964337835ae5fee0a699b86280a1ff3a0ec7
vlctyy <kharispowell@hotmail.com>
2025-05-07 09:15:52 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,53 +1,55 @@
-if getgenv().whenprismisntstrapping then return end -- Changed
 repeat task.wait() until game:IsLoaded()
+if getgenv().whenprismisntstrapping then return end -- Changed (was whenbloxisntstrapping)
 local exec, ver = identifyexecutor()
-if exec == 'Delta' and (ver:find('arm64') or ver:find('arm32')) then game.Players.LocalPlayer:Kick('Your executor doesn\'t support PrismStrap, We recommend using [krnl.cat] or other executors.') return end -- Delta fix your shit so I dont have to do this gang
-local cloneref = (table.find({'Xeno', 'Fluxus'}, identifyexecutor(), 1) or not cloneref) and function(ref)
+local cloneref = cloneref or function(ref)
     return ref
-end or cloneref :: (any)
+end
 --getgenv().developer = true
 getgenv().error = function(msg, lvl)
-    appendfile('PrismStrap/logs/error.txt', `{msg}\n`)
+    appendfile('PrismStrap/logs/error.txt', `{msg}\n`) -- Changed
     if getgenv().developer then
         task.spawn(getrenv().error, msg, lvl)
     end
 end
-getgenv().assert = function(a, b)
-    if not a then
-        error(b)
+getgenv().assert = function(statement, err)
+    if not statement then
+        error(err)
     end
 end
-if not isfolder('PrismStrap') or not isfolder('PrismStrap/logs') then
-    makefolder('PrismStrap')
+if not isfolder('PrismStrap') or not isfolder('PrismStrap/logs') then -- Changed
+    makefolder('PrismStrap') -- Changed
     for i: number, v: string in {'audios', 'core', 'images', 'logs', 'fonts', 'logs/cache'} do
-        makefolder(`PrismStrap/{v}`)
+        makefolder(`PrismStrap/{v}`) -- Changed
     end
-    writefile('PrismStrap/loader.lua', `loadstring(game:HttpGet('https://raw.githubusercontent.com/vlctyy/PrismStrap/refs/heads/main/loader.lua', true))()`)
-    writefile('PrismStrap/main.lua', `loadstring(game:HttpGet('https://raw.githubusercontent.com/vlctyy/PrismStrap/refs/heads/main/main.lua', true))()`)
-    writefile('PrismStrap/audios/oof sound.mp3', game:HttpGet('https://raw.githubusercontent.com/vlctyy/PrismStrap/refs/heads/main/audios/oof sound.mp3', true))
+    writefile('PrismStrap/loader.lua', `loadstring(game:HttpGet('https://raw.githubusercontent.com/vlctyy/PrismStrap/refs/heads/main/loader.lua', true))()`) -- Changed
+    writefile('PrismStrap/main.lua', `loadstring(game:HttpGet('https://raw.githubusercontent.com/vlctyy/PrismStrap/refs/heads/main/main.lua', true))()`) -- Changed
+    writefile('PrismStrap/audios/oof sound.mp3', game:HttpGet('https://raw.githubusercontent.com/vlctyy/PrismStrap/refs/heads/main/audios/oof sound.mp3', true)) -- Changed
 end
-if not isfile('PrismStrap/selected.txt') then
-    writefile('PrismStrap/selected.txt', 'fluent')
+if not isfile('PrismStrap/selected.txt') then -- Changed
+    writefile('PrismStrap/selected.txt', 'fluent') -- Changed
 end
-if not isfolder('PrismStrap/logs/cache') then
-    makefolder('PrismStrap/logs/cache')
+if not isfolder('PrismStrap/logs/cache') then -- Changed
+    makefolder('PrismStrap/logs/cache') -- Changed
 end
-if not isfolder('PrismStrap/songs') then
-    makefolder('PrismStrap/songs')
+if not isfolder('PrismStrap/songs') then -- Changed
+    makefolder('PrismStrap/songs') -- Changed
 end
-if not isfolder('PrismStrap/logs/blacklisted') then
-    makefolder('PrismStrap/logs/blacklisted')
+if not isfolder('PrismStrap/logs/blacklisted') then -- Changed
+    makefolder('PrismStrap/logs/blacklisted') -- Changed
 end
-if not isfolder('PrismStrap/logs/blacklisted') then -- (duplicate line from original script)
-    makefolder('PrismStrap/logs/blacklisted')
+if not isfolder('PrismStrap/logs/blacklisted') then -- Changed (duplicate line from original script)
+    makefolder('PrismStrap/logs/blacklisted') -- Changed
 end
 assert(setfflag, `Your executor ({identifyexecutor()}) doesn't have the required functions for this to work.`)
-writefile('PrismStrap/logs/error.txt', '')
+writefile('PrismStrap/logs/error.txt', '') -- Changed
 
 if getconnections then
-    for i,v in getconnections(game:GetService('LogService').MessageOut) do
+    for i,v in getconnections(cloneref(game:GetService('LogService')).MessageOut) do
+        v:Disable()
+    end
+    for i,v in getconnections(cloneref(game:GetService('ScriptContext')).Error) do
         v:Disable()
     end
 end
 
-return loadfile('PrismStrap/main.lua')()
+return loadfile('PrismStrap/main.lua')() -- Changed
